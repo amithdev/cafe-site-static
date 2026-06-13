ad075b47733548e465195d4115f0db9d0f36f19e
amithdev <91001572+amithdev@users.noreply.github.com>
2026-06-13 22:01:57 +0530

    Fix watermark visibility by removing CSS opacity

--- cache_bust.py
+++ cache_bust.py
@@ -5,9 +5,9 @@ for file in html_files:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
     
-    # Simple replace to add ?v=2
-    content = content.replace('href="static/css/style.css"', 'href="static/css/style.css?v=2"')
-    content = content.replace('href="../static/css/style.css"', 'href="../static/css/style.css?v=2"')
+    # Simple replace to add ?v=3
+    content = content.replace('href="static/css/style.css?v=2"', 'href="static/css/style.css?v=3"')
+    content = content.replace('href="../static/css/style.css?v=2"', 'href="../static/css/style.css?v=3"')
     
     with open(file, 'w', encoding='utf-8') as f:
         f.write(content)
